7d80a25b3b1a7e99b7b09f6c6d65a77b36cfbd1a
jcarleton <dr.jesc@gmail.com>
2023-06-20 21:09:34 -0700

    proto 5
    p5 enhancement

--- 005-smb-prototype.py
+++ 005-smb-prototype.py
@@ -13,8 +13,9 @@ import wandb
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
 from gym.wrappers import GrayScaleObservation
-from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecFrameStack, VecMonitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecFrameStack, VecMonitor, VecNormalize
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback, EvalCallback, CallbackList
 from smbprototypecustompolicy import CustomNetwork, CustomActorCriticPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
 from wandb.integration.sb3 import WandbCallback
@@ -57,6 +58,19 @@ class CustomReward(gym.Wrapper):
                 reward -= 50.0
         return state, reward / 10.0, done, info
 
+class trainingCallback(BaseCallback):
+    def __init__(self, interval, filepath, verbosity=1):
+        super(trainingCallback, self).__init__(verbosity)
+        self.interval = interval
+        self.filepath = filepath
+    def _init_callback(self) -> None:
+        if self.filepath is not None:
+            os.makedirs(self.filepath, exist_ok=True)
+    def _on_step(self) -> bool:
+        if self.n_calls % self.interval == 0:
+            model_path = os.path.join(self.filepath, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+        return True
 
 # set up the environment
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
@@ -64,21 +78,23 @@ env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = GrayScaleObservation(env, keep_dim=True)
 env = DummyVecEnv([lambda: env])
 env = VecFrameStack(env, 4, channels_order='last')
+env = VecNormalize(env, norm_obs=True, norm_reward=True)
 env = VecMonitor(env)
 
 # record video on interval
-env = VecVideoRecorder(env, f"videos/{wandb_run.id}", record_video_trigger=lambda x: x % 10000 == 0, video_length=2000)
+env = VecVideoRecorder(env, f"videos/{wandb_run.id}", record_video_trigger=lambda x: x % 50000 == 0, video_length=2000)
 # define model and parameters
-model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{wandb_run.id}", learning_rate=0.00001, n_steps=512)
+model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{wandb_run.id}", learning_rate=0.00001, n_epochs=20, n_steps=512)
 
 # learn the game!
 model.learn(
             total_timesteps=config["total_timesteps"],
-            callback=WandbCallback(
+            callback=[WandbCallback(
             gradient_save_freq=100000,
             model_save_path=f"models/{wandb_run.id}",
-            verbose=1,
-            ),)
+            verbose=2,
+            ),
+            trainingCallback(interval=100000, filepath=f"models/{wandb_run.id}")],)
 
 
 PPO_path = os.path.join('Training', 'Saved Models', 'PPO_SuperMario_2M')
