779d40721bf899e4632b8491e1c5bbfbc3bb6d8d
jcarleton <dr.jesc@gmail.com>
2023-07-24 19:51:52 -0700

    p8

--- prototypes/008-smb-prototype.py
+++ prototypes/008-smb-prototype.py
@@ -10,29 +10,61 @@ import os
 import gym_super_mario_bros
 import gym
 import wandb
-import cv2
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 from nes_py.wrappers import JoypadSpace
-from gym.wrappers import GrayScaleObservation, FrameStack
-from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecFrameStack, VecMonitor, VecNormalize
+from gym.wrappers import GrayScaleObservation, ResizeObservation
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecFrameStack, VecMonitor, VecNormalize, VecEnv
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback, EvalCallback, CallbackList
+from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv, ClipRewardEnv, NoopResetEnv
 from smbcustomnn001 import CustomNetwork, CustomActorCriticPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
 from wandb.integration.sb3 import WandbCallback
 
+
 # define custom neural net, timesteps, environment name
 config = {
         "policy_type": CustomActorCriticPolicy,
-        "total_timesteps": 100000,
+        "total_timesteps": 250000,
         "env_name": 'SuperMarioBros-v0',
 }
+
+
+# initialize wandb with project name, config, and requirements for metrics
+wandb_run = wandb.init(
+    project="smb-ai-run-01",
+    config=config,
+    sync_tensorboard=True,
+    monitor_gym=True,
+    save_code=True,
+    settings=wandb.Settings(code_dir=".")
+)
 
 
 # define training checkpoint dir
 checkpoint_dir = './training/'
 # define logging dir
 logging_dir = './log/'
+
+
+CUSTOM_ACTIONS = [
+    ['NOOP'],
+    ['right'],
+    ['right', 'A'],
+    ['right', 'B'],
+    ['right', 'B', 'A'],
+    ['B', 'right', 'A'],
+    ['A'],
+    ['left'],
+    ['left', 'A'],
+    ['left', 'B'],
+    ['left', 'A', 'B'],
+    ['B', 'left', 'A'],
+    ['down'],
+    ['up'],
+    ['A', 'up'],
+]
+
 
 class CustomReward(gym.RewardWrapper):
     def __init__(self, env):
@@ -54,7 +86,7 @@ class CustomReward(gym.RewardWrapper):
             elif info['status'] == 'fireball':
                 reward += 200.0
             elif info['x_pos'] > 0:
-                reward += info['x_pos']
+                reward += info['x_pos'] * 5.0
             elif info['time'] > 0:
                 reward -= (400 - info['time']) % 3
             else:
@@ -77,78 +109,146 @@ class trainingCallback(BaseCallback):
         return True
 
 
-class ResizeObservation(gym.ObservationWrapper):
-    """Downsample the image observation to a square image. """
-    def __init__(self, env, shape):
-        super().__init__(env)
-        if isinstance(shape, int):
-            self.shape = (shape, shape)
-        else:
-            self.shape = tuple(shape)
-
-        obs_shape = self.shape + self.observation_space.shape[2:]
-        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
-
-    def observation(self, observation):
-        observation = cv2.resize(observation, self.shape, interpolation=cv2.INTER_AREA)
-        return observation
-
-bestModelCallback = EvalCallback(callback_on_new_best=saveModelNow())
-
-# set up the environment
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = CustomReward(env)
-env = JoypadSpace(env, COMPLEX_MOVEMENT)
-env = GrayScaleObservation(env, keep_dim=True)
-env = DummyVecEnv([lambda: env])
-env = VecFrameStack(env, 4, channels_order='last')
-# normalize observation or reward, uses moving average
-# env = VecNormalize(env, norm_obs=True, norm_reward=False)
-env = VecMonitor(env)
+def make_smb_env():
+    # set up the environment
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = CustomReward(env)
+    # env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = JoypadSpace(env, CUSTOM_ACTIONS)
+    env = NoopResetEnv(env, noop_max=30)
+    env = GrayScaleObservation(env, keep_dim=True)
+    env = ResizeObservation(env, (84, 84))
+    env = MaxAndSkipEnv(env, skip=4)
+    env = ClipRewardEnv(env)
+    env = DummyVecEnv([lambda: env])
+    env = VecFrameStack(env, 4, channels_order='last')
+    # normalize observation or reward, uses moving average
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+    env = VecMonitor(env)
+    return env
 
 
-steps = [512, 1024, 2048]
-# epochs = [5, 10, 15, 20]
-lr = [0.00001, 0.0001, 0.001, 0.01, 0.1]
-ent = [0.00001, 0.0001, 0.001, 0.01]
+env = make_smb_env()
 
-for entco in ent:
-    for lrate in lr:
-        for step in steps:
+# record video on interval
+env = VecVideoRecorder(env, f"videos/{wandb_run.id}", record_video_trigger=lambda x: x % 50000 == 0, video_length=2000)
 
-            # initialize wandb with project name, config, and requirements for metrics
-            wandb_run = wandb.init(
-                project="smb-ai-sweep3",
-                config=config,
-                sync_tensorboard=True,
-                monitor_gym=True,
-                save_code=True,
-                settings=wandb.Settings(code_dir=".")
+# define model and parameters
+model = PPO(config["policy_type"],
+            env,
+            verbose=1,
+            tensorboard_log=f"runs/{wandb_run.id}",
+            learning_rate=0.00001,
+            n_epochs=20,
+            n_steps=256,
+            batch_size=256,
+            vf_coef=0.5,
+            ent_coef=0.01,
+            max_grad_norm=0.5
             )
 
-            # record video on interval
-            env = VecVideoRecorder(env, f"videos/{wandb_run.id}", record_video_trigger=lambda x: x % 50000 == 0, video_length=2000)
 
-            # define model and parameters
-            model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{wandb_run.id}", ent_coef=entco, learning_rate=lrate, n_epochs=epoch, n_steps=step)
+# learn the game!
+model.learn(
+            total_timesteps=config["total_timesteps"],
+            callback=[WandbCallback(
+            gradient_save_freq=int(config["total_timesteps"]/20),
+            model_save_path=f"models/{wandb_run.id}",
+            verbose=2,
+            ),
+            trainingCallback(interval=config["total_timesteps"]/10, filepath=f"models/{wandb_run.id}")],)
 
-            # learn the game!
-            model.learn(
-                        total_timesteps=config["total_timesteps"],
-                        callback=[WandbCallback(
-                        gradient_save_freq=int(config["total_timesteps"]/20),
-                        model_save_path=f"models/{wandb_run.id}",
-                        verbose=2,
-                        ),
-                        trainingCallback(interval=config["total_timesteps"]/10, filepath=f"models/{wandb_run.id}")
-                        ],)
+env.close()
 
-            # save the final model
-            PPO_path = os.path.join('Training', 'Saved Models', f"PPO_SuperMario_1M/{wandb_run.id}")
-            model.save(PPO_path)
 
-            # evaluate the model
-            evaluate_policy(model, env, n_eval_episodes=10, render=False)
-            # terminate the WandB session
-            wandb_run.finish()
-            env.reset()
+# define model and parameters
+model = PPO(config["policy_type"],
+            env,
+            verbose=1,
+            tensorboard_log=f"runs/{wandb_run.id}",
+            learning_rate=0.0001,
+            n_epochs=20,
+            n_steps=256,
+            batch_size=256,
+            vf_coef=0.5,
+            ent_coef=0.01,
+            max_grad_norm=0.5
+            )
+
+
+# learn the game!
+model.learn(
+            total_timesteps=config["total_timesteps"],
+            callback=[WandbCallback(
+            gradient_save_freq=int(config["total_timesteps"]/20),
+            model_save_path=f"models/{wandb_run.id}",
+            verbose=2,
+            ),
+            trainingCallback(interval=config["total_timesteps"]/10, filepath=f"models/{wandb_run.id}")],)
+
+env.close()
+
+
+# define model and parameters
+model = PPO(config["policy_type"],
+            env,
+            verbose=1,
+            tensorboard_log=f"runs/{wandb_run.id}",
+            learning_rate=0.001,
+            n_epochs=20,
+            n_steps=256,
+            batch_size=256,
+            vf_coef=0.5,
+            ent_coef=0.01,
+            max_grad_norm=0.5
+            )
+
+
+# learn the game!
+model.learn(
+            total_timesteps=config["total_timesteps"],
+            callback=[WandbCallback(
+            gradient_save_freq=int(config["total_timesteps"]/20),
+            model_save_path=f"models/{wandb_run.id}",
+            verbose=2,
+            ),
+            trainingCallback(interval=config["total_timesteps"]/10, filepath=f"models/{wandb_run.id}")],)
+
+env.close()
+
+
+# define model and parameters
+model = PPO(config["policy_type"],
+            env,
+            verbose=1,
+            tensorboard_log=f"runs/{wandb_run.id}",
+            learning_rate=0.01,
+            n_epochs=20,
+            n_steps=256,
+            batch_size=256,
+            vf_coef=0.5,
+            ent_coef=0.01,
+            max_grad_norm=0.5
+            )
+
+
+# learn the game!
+model.learn(
+            total_timesteps=config["total_timesteps"],
+            callback=[WandbCallback(
+            gradient_save_freq=int(config["total_timesteps"]/20),
+            model_save_path=f"models/{wandb_run.id}",
+            verbose=2,
+            ),
+            trainingCallback(interval=config["total_timesteps"]/10, filepath=f"models/{wandb_run.id}")],)
+
+
+# save the final model
+PPO_path = os.path.join('Training', 'Saved Models', f"PPO_SuperMario_1M/{wandb_run.id}")
+model.save(PPO_path)
+
+# evaluate the model
+evaluate_policy(model, env, n_eval_episodes=10, render=False)
+# terminate the WandB session
+wandb_run.finish()
+env.close()
